13e4eadb7a90cd8b4ca09e733fbb185d214469f0
bhattarairazu <razubhattarai87@gmail.com>
2021-01-21 20:42:40 +0545

    Apache Kafka Practice

---
+++ src/test/java/com/razu/apacheKafka/ApacheKafka/ApacheKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.razu.apacheKafka.ApacheKafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
